adf535c2e79105175e805839e3223a5fadc98da2
Kissmark Rivas <rivaskissmark@gmail.com>
2022-08-14 09:29:57 -0500

    counterapp created

---
+++ src/FirstApp.jsx
@@ -0,0 +1,14 @@
+export const FirstApp = ({title, subTitle}) => {
+  
+  // const newMessage = {nombre:'Kissmark',apellidoPaterno:'Rivas',apellidoMaterno:'Moreno'}
+  // console.log(props);
+  return (
+    <>
+      <h1>{  title }</h1>
+      <h1>{  subTitle }</h1>
+    </>
+  )
+}
+// const newMessage = {nombre:'Kissmark',apellidoPaterno:'Rivas',apellidoMaterno:'Moreno'}
+// const newMessage = ['Kissmark','Rivas','Moreno']
+// <h1>{ JSON.stringify(newMessage) }</h1>
