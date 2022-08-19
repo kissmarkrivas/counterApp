7f32e1c23427e2cedd2f304f1ddcb61e4ad4130c
Kissmark Rivas <rivaskissmark@gmail.com>
2022-08-18 21:09:47 -0500

    test snapshot andmount match and props

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CounterApp } from './CounterApp'
+import { FirstApp } from './FirstApp'
 
+import {CounterApp} from './CounterApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CounterApp value={10}/>
+    {/* <FirstApp title="Hola, Soy vegeta"/> */}
   </React.StrictMode>
 )
